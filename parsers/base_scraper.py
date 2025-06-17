48c6c434cd3d3c8f7dff9868d99eb5d05f5d614f
Roman <romanbur54@gmail.com>
2025-06-17 10:42:20 +0300

    create base parser

---
+++ parsers/base_scraper.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium import webdriver
+
+
+class BaseScraper(ABC):
+    def __init__(self, driver: WebDriver, url: str):
+        self.driver = driver
+        self.url = url
+
+
+
+    @abstractmethod
+    def get_title(self):
+        pass
+
+    # @abstractmethod
+    # def get_all_page(self):
+    #     pass
+    #
+    # @abstractmethod
+    # def get_price(self, html_pages: list[str]) -> list[str]:
+    #     pass
