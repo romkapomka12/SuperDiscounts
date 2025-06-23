e92da61950fc28a7b3bdd1e36b39b167699822c8
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2025-06-23 13:29:18 +0300

    Added keyboard for selecting store and selected products

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -3,11 +3,23 @@ from aiogram.utils import keyboard
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from pandas.core.dtypes.cast import construct_1d_arraylike_from_scalar
 
-main = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Catalog", callback_data="catalog")],
-    [InlineKeyboardButton(text="Musor", callback_data="basket"),
-    InlineKeyboardButton(text="Contact", callback_data="contacts")]
-])
+# main = InlineKeyboardMarkup(inline_keyboard=[
+#     [InlineKeyboardButton(text="АТБ", callback_data="shop-atb")],
+#     [InlineKeyboardButton(text="Новус", callback_data="shop-novus")],
+#     [InlineKeyboardButton(text="Сільпо", callback_data="shop-silpo")],
+#     [InlineKeyboardButton(text="Фора", callback_data="shop-fora")],
+#     [InlineKeyboardButton(text="Всі магазини", callback_data="shop-all")]
+# ])
+
+user_favorites = {}
+
+buttons = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Обрати магазин")],[KeyboardButton(text="Обрані товари")]
+
+
+],
+                     resize_keyboard=True,
+                     input_field_placeholder="пошук товарів")
 
 
 settings = InlineKeyboardMarkup(inline_keyboard=[
@@ -16,19 +28,20 @@ settings = InlineKeyboardMarkup(inline_keyboard=[
 ])
 
 shop = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="АТБ", callback_data="shop_atb")],
+    [InlineKeyboardButton(text="АТБ", callback_data="shop-atb")],
     [InlineKeyboardButton(text="Новус", callback_data="shop_novus")],
     [InlineKeyboardButton(text="Сільпо", callback_data="shop_silpo")],
     [InlineKeyboardButton(text="Фора", callback_data="shop_fora")],
     [InlineKeyboardButton(text="Всі магазини", callback_data="shop_fora")]
 ])
 
-shops = ["ATB", "NOVUS", "SILPO", "FORA", "Всі магазины"]
+# shops = ["ATB", "NOVUS", "SILPO", "FORA", "Всі магазины"]
 
-async  def inline_shops():
-    keyboard = InlineKeyboardBuilder()
-    for shop in shops:
-        keyboard.add(InlineKeyboardButton(text=shop, callback_data=shop))
-    return keyboard.adjust(2).as_markup()
+# async  def inline_shops():
+#     keyboard = InlineKeyboardBuilder()
+#     for shop in shops:
+#         keyboard.add(InlineKeyboardButton(text=shop, callback_data=shop))
+#     return keyboard.adjust(2).as_markup()
+
 
 
