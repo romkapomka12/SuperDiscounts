f909c8343a047ebfadb0e79e4e226bbb29472e42
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2025-06-23 13:32:59 +0300

    Added asynchronous main function to process products and start polling in the telegram bot

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ import os
 import logging
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-from bot.handlers import router
-from parsers import atb
-
+# from bot.handlers import router as show_products
+from config import logger
+from data.save_product import load_atb_products_from_csv
 load_dotenv()
 
 API_TOKEN = os.environ["API_TOKEN"]
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
-
+products = load_atb_products_from_csv()
 
 async def main():
-    dp.include_routers(router)
+    from bot.handlers import router as show_products
+    dp.include_routers(show_products)
+    print(f"✅ Завантажено товарів: {len(products)}")
+    print("Обробляється callback shop-atb")
+    print("🔍 Тип першого продукту:", type(products[0]))
+    print("📦 Перший товар:", products[0])
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    atb()
-    # logging.basicConfig(level=logging.INFO)
-    # try:
-    #     asyncio.run(main())
-    # except KeyboardInterrupt:
-    #     print("exit")
+
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("exit")
